e7c3c3b1445179edc7ead6f4f1ad287aa7fd6dae
Royy526 <rodrigodj526@gmail.com>
2026-08-24 10:59:05 -0600

    movimiento de plataformas (incompleto))

---
+++ GameDesign/Assets/RodrigoDiaz/PlataformMovement.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class PlataformMovement : MonoBehaviour
+{
+    public float Speed = 5f;
+    private Vector3 direction = Vector3.up;
+    public int maxY = 5;
+    public int minY = 1;
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        transform.Translate(direction * Time.deltaTime * Speed);
+    }
+
+    public void MoveUp()
+    {
+        if (transform.position.y <= minY)
+        {
+            direction = Vector3.up;
+        }
+    }
+
+    public void MoveDown()
+    {
+        if (transform.position.y >= maxY)
+        {
+            direction = Vector3.down;
+        }
+    }
+}
