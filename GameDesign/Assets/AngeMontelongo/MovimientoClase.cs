545796f6649a844e9506fa27bd9f48d248eff7e1
Angel <3plcachamp@gmail.com>
2026-08-24 10:51:07 -0600

    MovimientoCubo

---
+++ GameDesign/Assets/AngeMontelongo/MovimientoClase.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class MovimientoClase : MonoBehaviour
+{
+    public float speed = 2.0f;
+    public float distance = 5.0f;
+
+    private Vector3 startPosition;
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movimiento = Mathf.PingPong(Time.time * speed, distance * 2) - distance;
+        transform.position = startPosition + new Vector3(movimiento, 0f, 0f);
+    }
+}
