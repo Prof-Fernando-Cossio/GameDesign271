a3c21e45f1438a4933fef317079f9621e5c1dc98
Alex <ju4n43rn4nde3z@gmail.com>
2026-08-24 10:55:32 -0600

    Intento de movimiento (no sale ayuda)

---
+++ GameDesign/Assets/Assets/AldoHeal/ScriptHeal/Movimiento.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour
+{
+    public float speed = 2f;
+    public Vector3 targeted;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, targeted, speed * Time.deltaTime);
+    }
+}
