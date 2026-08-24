accfda5e07d00e908cec5efad216c1714ff9ecb9
Deafualt profile <resendizdiegog@gmail.com>
2026-08-24 10:57:35 -0600

    Avance mov bloque

---
+++ GameDesign/Assets/DiegoResendiz/move.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    private float Velocidad;
+    public float direccion = 5.0f;
+    public float distancia = 5.0f;
+    float origen;
+    private void Start()
+    {
+        origen = transform.position.x;
+        Velocidad = -direccion;
+    }
+
+    private void Update()
+    {
+        if (origen - transform.position.x> distancia)
+        {
+            Velocidad = direccion;
+        }
+        else if (origen - transform.position.x < -distancia) 
+        {
+            Velocidad = -direccion;
+        }
+        transform.Translate(0, Velocidad * Time.deltaTime, 0);
+    }
+
+}
