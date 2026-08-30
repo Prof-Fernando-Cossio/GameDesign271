3b2c9ffed9ed0e31363e0c4e2459e25c09dc1598
Roy <rodrigodj526@gmail.com>
2026-08-30 13:44:21 -0600

    Ejercicio variables terminado

--- GameDesign/Assets/RodrigoDiaz/EjercicioVariables.cs
+++ GameDesign/Assets/RodrigoDiaz/EjercicioVariables.cs
@@ -7,21 +7,25 @@ using UnityEngine.Serialization;
 
 public class EjercicioVariables : MonoBehaviour
 {
-    [SerializeField] private float _movementSpeed = 5f;
-    internal int level = 0;
-    protected int baseDamage = 10;
-    private int _playerHealth = 100;
-    [SerializeField] private Rigidbody rb;
-    private bool _isAlive = true;
-    //private Array<> 
-    //[SerializeField][Min(0)] private float _damegeRange = 100;
-    internal string playerName;
-    protected float movementSpeed = 1.0f;
-    private MeshRenderer rbMesh;
-    [Tooltip("Esta variabel modifica el volumen de los sonidos del juego")][SerializeField] private int _gameVolume = 100;
-    private bool _canAttack = false;
-    public static GameManager Instance { get; private set; }
-    private List<> _inventoryItems;
-    [FormerlySerializedAs("_speed")][SerializeField] private float speed = 1f;
-
+    
+    //1. [SerializeField] private float _movementSpeed = 5f;
+    //2. internal int level = 0;
+    //3. protected int baseDamage = 10;
+    //4. private int _playerHealth = 100;
+    //5. [SerializeField] private Rigidbody rb;
+    //6. private bool _isAlive = true;
+    //7. internal List<int> _save; 
+    //8. [SerializeField][Range(0,100)] private float _damegeRange = 100;
+    //9. internal string playerName;
+    //10. protected float movementSpeed = 1.0f;
+    //11. private MeshRenderer rbMesh;
+    //12. [Tooltip("Esta variabel modifica el volumen de los sonidos del juego")][SerializeField] private int _gameVolume = 100;
+    //13. private bool _canAttack = false;
+    //14. public static GameManager Instance { get; private set; }
+    //15. private List<> _inventoryItems;
+    //16. [FormerlySerializedAs("_speed")][SerializeField] private float speed = 1f;
+    //17. private Vector3 _playerPosition;
+    //18. public int MaxPlayers = 100;
+    //19. [SerializedField] protected int enemyDetectionDistance;
+    //20. [SerializedField] private AudioSource _audio;
 }
