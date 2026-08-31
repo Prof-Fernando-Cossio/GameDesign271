192c602af2f1996b32351c16c99b02993338baf4
RogelioAMERIKE <cdmx3073@amerike.edu.mx>
2026-08-31 10:58:08 -0600

    MetodosPRuebalel

---
+++ GameDesign/Assets/AndreV/Metodos.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+
+
+
+public class Metodos : MonoBehaviour
+{
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        ad
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public float _playerLife = 100f;
+
+    private string
+    //1 Metodo que reduzca la vida actual de un jugador, recibe la cantida de daño como parametro
+    public void ReduceLife (int damageCantidad)
+    {
+        
+    }
+    //2 Crea un metodo que verifique si el jugador esta vivo segun su vida actual
+    public float _playerLife = 100;
+    public bool IsPlayerAlive()
+    {
+        //turn myPlayer.Health > 0;
+        return _playerLife > 0;
+    }
+
+    //3 Crear un metodo que calcule la distancia entre dos posicion es en el espacio
+
+    //4 Crear un metodo que devuelve la direccion  normalizada desde un origen hacia un destino 
+
+    //5 Crear un metodo que devuelva el nombre actual del jugador
+
+    //6 Crear un metodo que cuente la cantidad de enemigos contenidos en una lista 
+
+    //7 Crea un metodo que encuentra el enemigo mas cercano al jugador dentro de una lista
+
+    //8 Crea un metodo que reciba velocidad y direccion y mueva al jugador
+
+    //9 Crea un metodo que convierta un angulo expresado en grados a radianess
+
+    //10 Crea un metodo que intente obtener al jugador mas cercano dentro de un rango dado, indicado si se encontro uno y devolviendo al jugador obtenido
+
+    //11 Crea un metodo que intente convertir un texto a entero indicando si la conversion fue exitosa y devolviendo el valor obtenido 
+
+    //12 Crea un metodo que reciba un angulo en grados y devuelva la rotacion correspondiente como cuaternion 
+
+    //13 Crea un metodo que llene una lista con todos los enemigos que se ecnuentren en un area dada
+
+    //14 Crea un metodo que reinicie la posicion del jugador a un punto de aparicion 
+
+    //15 En una clase base cra un metodo diseñado para ser redefinido por las clases hijas
+
+    //16 En una clase hija redefine el metodo del ejercicio anterior para proporcionar un comportamiento especifico 
+
+    //17 En la clase hija llama al comportamiento originalo del metodo definido en la clase base
+
+    //18 Crea un metodo que devuela el porcentaje de vida actual de un jugador respecto a su vida maxima
+
+    //19 Crea un metodo que determine si un enemigo puede esquigvar un ataque segun una probabilidad dada
+
+    //20 Crea un metodo que aplique una fuerza en una direccion a un Rigidbody
+}
