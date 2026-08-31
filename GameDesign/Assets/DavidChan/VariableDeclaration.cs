6ce12dce040d1ff84807d8dfa67fd8ee7dba5a32
David Chan <devKomor@gmail.com>
2026-08-31 10:58:52 -0600

    Ejercicio de metodos

--- GameDesign/Assets/DavidChan/VariableDeclaration.cs
+++ GameDesign/Assets/DavidChan/VariableDeclaration.cs
@@ -11,12 +11,12 @@ public class VariableDeclaration : MonoBehaviour
     // 2. El nivel actual del jugador que otros sistemas del juego
     //    necesitan leer.
 
-    internal int playerLevel = 0;
+    internal int _playerLevel = 0;
 
     // 3. Un daño base que solo las clases derivadas pueden usar.
     //    Valor inicial de 10
 
-    protected float baseDamage = 10f;
+    protected float _baseDamage = 10f;
 
     // 4. La vida actual del jugador. Ningún Otro script 
     //    debe poder modificarla directamente.
@@ -68,7 +68,7 @@ public class VariableDeclaration : MonoBehaviour
 
     // 13. Un valor verdadero/falso que indica si el enemigo
     // puede atacar. Solo este script lo usa.
-    
+
     private bool _canEnemyAttack;
 
     // 14. La instancia global de GameManager a la que cualquier
@@ -101,7 +101,7 @@ public class VariableDeclaration : MonoBehaviour
     // 19. Una distancia de deteccion enemiga que las clases derivadas
     //     puedan reutilizar y que sea editable desde el Inspector.
 
-    [SerializeField]protected float _enemyDetection = 8.5f;
+    [SerializeField] protected float _enemyDetection = 8.5f;
 
     // 20. Una referencia a AudioSource que solo este script usa
     //     y que sea editable desde el Inspector.
@@ -111,12 +111,76 @@ public class VariableDeclaration : MonoBehaviour
 
     void Start()
     {
-        
+
     }
 
-    
+
     void Update()
     {
-        
+
     }
+
+    //public void FindPlayer(string playerName, out Player foundPlayer) //El keyword "out nos permite regresar 2  tipos de variables por metodo"
+    //{
+
+    //}
+
+    // MÉTODOS, OUT VIRTUAL/OVERRIDE 
+
+    //1. Crea un metodo que reduzca la vida actual de un jugador.
+    //   Recibe la cantidad de daño como parámetro
+
+    public float LoseHealth(float damage)
+    {
+        _playerHealth = _playerHealth - damage; //Math.f para que es???
+    }
+
+    //2. Crea un método que verifique si el jugador esta vivo según su vida actual.
+
+    public bool IsPlayerAlive() //Poniendo parametro (Player currentPlayer)
+    {
+
+        //retun currentPlayer.Health > 0;
+
+        //Sin parametro
+
+        return _playerHealth > 0;
+    }
+
+    //3. Crea un método que calcule la distancia entre dos posiciones en el espacio
+
+    //4. Crea un método que devuelva la dirección normalizada desde un origen hacia un destino.
+
+    //5. Crea un metodo que devuelva el nombre actual del jugador.
+
+    //6. Crea un método que cuente la cantidad de enemigos contenidos en una lista.
+
+    //7. Crea un método que encuentre el enemigo más cercano al jugador dentro de una lista.
+
+    //8. Crea un método que reciba la velocidad y dirección y mueva al jugador
+
+    //9. Crea un método que convierta un ángulo expresado en grados a radianes.
+
+    //10. Crea un método que intente obtener al jugador más cercano dentro de un rango dado, indicando si se encontró uno y devolviendo el jugador obtenido
+
+    //11. Crea un método que intente convertir un texto a entero, indicando si la conversión fue exitosa y devolviendo el valor obtenido.
+
+    //12. Crea un método que reciba un ángulo en grados y devuelva la rotación correspondiente como cuaternión
+
+    //13. Crea un método que llene una lista con todos los enemigos que se encuentren en un área dada.
+
+    //14. Crea un método que reinicie la posicion del jugador a un punto de aparición.
+
+    //15. En una clase base, crea un método diseñado para ser redefinido por las clases hijas.
+
+    //16. En una clase hija, redefine el método del ejercicio anterior para proporcionar un comportamiento especifico.
+
+    //17. En la clase hija, llama al comportamiento original del método definido en la clase base.
+
+    //18. Crea un método que devuelva el porcentaje de vida actual de un jugador respecto a su vida máxima.
+
+    //19. Crea un método que determine si un enemigo puede esquivar un ataque según una probabilidad dada.
+
+    //20. Crea un método que aplique una fuerza en una direccion a un Rigidbody
+
 }
