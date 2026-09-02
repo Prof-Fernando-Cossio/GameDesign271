8c233bfedc9332fd906305c028589b956278581c
Otto <prograbugs@gmail.com>
2026-09-01 21:54:53 -0600

    Methods Practice Advancement 2

--- GameDesign/Assets/LuisOctavioRiveraCliment/Scripts/MethodsPractice.cs
+++ GameDesign/Assets/LuisOctavioRiveraCliment/Scripts/MethodsPractice.cs
@@ -1,6 +1,13 @@
 using NUnit.Framework;
 using UnityEngine;
 using System.Collections.Generic;
+using System;
+
+public class Player : MonoBehaviour
+{
+    public string Name;
+    public int Health;
+}
 
 public class MethodsPractice : MonoBehaviour
 {
@@ -14,13 +21,12 @@ public class MethodsPractice : MonoBehaviour
         return _currentHealth - _damageDealt;
     }
 
+
     //2
-    /*
     private bool IsPlayerAlive(Player myPlayer)
     {
         return myPlayer.Health > 0;
     }
-    */
 
     //3
     private float CalculateDistance(Vector3 _originPoint, Vector3 _targetPosition)
@@ -36,12 +42,10 @@ public class MethodsPractice : MonoBehaviour
     }
 
     //5
-    /*
     private string GetPlayerName(Player _myPlayer)
     {
         return _myPlayer.Name;
     }
-    */
 
     //6
     private int CountActiveEnemies(List<GameObject> _enemies)
@@ -49,18 +53,59 @@ public class MethodsPractice : MonoBehaviour
         return _enemies.Count;
     }
 
+    //7
     private void FindEnemyCloserToPlayer(List<GameObject> _enemies, Player _myPlayer)
     {
         float _closestDistance = 100f;
-        GameObject _closerEnemy;
-        foreach(GameObject _enemy in _enemies)
+        GameObject _closerEnemy = _enemies[0];
+        foreach (GameObject _enemy in _enemies)
         {
             float distance = Vector3.Distance(_myPlayer.transform.position, _enemy.transform.position);
-            if(distance > _closestDistance)
+            if (distance < _closestDistance)
             {
                 _closestDistance = distance;
                 _closerEnemy = _enemy;
             }
         }
+        Debug.Log(_closerEnemy);
+        Debug.Log(_closestDistance);
     }
+
+    //8
+    private void CharacterMovement(float _speed, Vector3 _direction, CharacterController _playerController)
+    {
+        _playerController.Move(_direction * _speed * Time.deltaTime);
+    }
+
+    //9
+    private void ConvertToRadians(float _degrees)
+    {
+        Debug.Log((Mathf.PI * _degrees) / 180);
+    }
+
+    //10
+    private Player FindEnemyCloserToPlayer(List<Player> _players, Vector3 _pointToGo, float _rangeToAnalize)
+    {
+        float _closestDistance = 100f;
+        Player _closerPlayer = _players[0];
+        foreach (Player _player in _players)
+        {
+            float _distance = Vector3.Distance(_player.transform.position, _pointToGo);
+            if (_distance < _closestDistance && _distance < _rangeToAnalize)
+            {
+                _closestDistance = _distance;
+                _closerPlayer = _player;
+            }
+        }
+        return _closerPlayer;
+    }
+
+    //11
+    private bool ConvertStringToInt(string _textToConvert)
+    {
+        return Int32.TryParse(_textToConvert, out _);
+    }
+
+    //12
+
 }
