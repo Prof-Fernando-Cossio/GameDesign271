6a49038cb092f55085fcdf39216d10b0f25d674b
Angel <3plcachamp@gmail.com>
2026-09-03 13:11:20 -0600

    Ejercicios3completo

--- GameDesign/Assets/AngeMontelongo/ejercicios4.cs
+++ GameDesign/Assets/AngeMontelongo/ejercicios4.cs
@@ -1,55 +1,95 @@
-using System.Data;
-using System.Runtime.CompilerServices;
-using TMPro.Examples;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Rendering;
 
-public class ejercicios4 : MonoBehaviour
+public class EjerciciosPropiedades : MonoBehaviour
 {
-    private float _currentHealth = 100f;
-    private float _maxHealth = 100f;
-    private float _movementSpeed;
-    private int _damage;
-    private float _distancetoPlayer;
-    private float _attackRange = 5;
-    private float _volume;
-    [SerializeField] private float _stamina;
-
-
-
-
+    // 1
     public int Health { get; private set; }
+
+    // 2
     public bool IsDead => Health <= 0;
+
+    // 3
     public string PlayerName { get; init; }
+
+    // 4
     public int Coins { get; set; }
-    public float HeathPercentage => _currentHealth / _maxHealth * 100f;
+
+    // 5
+    private float _currentHealth = 100;
+    private float _maxHealth = 100;
+
+    public float HealthPercent => (_currentHealth / _maxHealth) * 100;
+
+    // 6
+    private float _movementSpeed;
     public float MovementSpeed => _movementSpeed;
+
+    // 7
+    private int _damage;
+
     public int Damage
     {
-        get {  return _damage; }
+        get { return _damage; }
         set { _damage = Mathf.Clamp(value, 0, 100); }
     }
+
+    // 8
     public static int PlayerCount { get; private set; }
+
+    // 9
     public int Experience { get; private set; }
+
+    // 10
+    [SerializeField] private float _stamina;
     public float Stamina => _stamina;
-    public bool CanAttack => _distancetoPlayer <= _attackRange;
+
+    // 11
+    private float _distanceToPlayer;
+    private float _attackRange = 5;
+
+    public bool CanAttack => _distanceToPlayer <= _attackRange;
+
+    // 12
+    private float _volume;
+
     public float Volume
     {
         get { return _volume; }
-        set {
-            if (value < 0) Volume = 0;
-            else _volume = value;
+
+        set
+        {
+            if (value < 0)
+                _volume = 0;
+            else
+                _volume = value;
         }
     }
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // 13
+    public System.DateTime CreationDate { get; } = System.DateTime.Now;
+
+    // 14
+    private int _inventoryItems;
+    private int _maxInventoryItems = 10;
+
+    public bool InventoryFull => _inventoryItems >= _maxInventoryItems;
+
+    // 15
+    public int MaxLevel { get; init; }
+
+    // 16
+    public Rigidbody rb;
+    public float HorizontalSpeed => rb.velocity.x;
+
+    // 17
+    public float Energy { get; private set; }
+
+    // 18
+    public Vector3 CurrentPosition => transform.position;
+
+    // 19
+    public int InventoryItems => _inventoryItems;
+
+    // 20
+    public bool IsRunning => rb.velocity.magnitude > 0;
 }
