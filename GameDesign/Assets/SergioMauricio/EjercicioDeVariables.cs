358bdaa4674df68c8c5e15dbeec74914fb5f754d
Sergio Mauricio Camacho Saavedra <sergiomcamachos12@gmail.com>
2026-09-03 07:45:57 -0600

    Ejercicio Métodos completo

--- GameDesign/Assets/SergioMauricio/EjercicioDeVariables.cs
+++ GameDesign/Assets/SergioMauricio/EjercicioDeVariables.cs
@@ -3,12 +3,13 @@ using System.Collections;
 using UnityEngine;
 using System.Collections.Generic;
 using System;
+using UnityEngine.Serialization;
 
 public class EjercicioDeVariables : MonoBehaviour
 {
     //1° Un valor de velocidad que solo este script usa que sea editable desde el Inspector. valor inicial de 5
 
-    [SerializeField] private float _speedObject = 5;
+    [SerializeField] private float _speedObject = 5f;
 
     //2° El nivel actual del jugador que otros sistemas del juego necesitan leer
 
@@ -16,7 +17,7 @@ public class EjercicioDeVariables : MonoBehaviour
 
     //3° Un daño base que solo las clases derivadas pueden usar, valor inicial de 10
 
-    protected int BaseDamage = 10;
+    protected int _baseDamage = 10;
 
     //4° La vida actual del jugador. Ningun otro script debe poder modificarla directamente
 
@@ -24,52 +25,52 @@ public class EjercicioDeVariables : MonoBehaviour
 
     //5° Una referencia a Rigidbody que solo este script usa y sea editable desde el inspector
 
-    [SerializeField] private Rigidbody _rb;
+    [SerializeField] private Rigidbody _rigidBody;
 
     //6° Un valor verdadero/falso que indique si el jugador sigue vivo. Solo este script lo usa.
 
-    [SerializeField] private bool _isAlive;
+    private bool _isAlive;
 
     //7° Un indice de guardado que ningun otro script fuera de este ensamblado pueda acceder.
 
-    internal int CheckPoint;
+    internal int _checkPoint;
 
     //8° Un rango de ataque que solo este script usa, es editable desde el Inspector y limitado entre 0 y 100
 
     [Range(0, 100)]
-    [SerializeField] private float _rangeAttack;
+    [SerializeField] private float _rangeAttack = 10f;
 
     //9° El nombre atual del jugador que cualquier sistema del juego debe poder leer
 
-    internal string _playerName;
+    public string PlayerName;
 
     //10° Una velocidad de movimiento que las clases derivadas (enemigos, aliados, jefes, etc.) puedan reutilizar
 
     protected float _baseSpeed;
 
     //11°Una referencia a MeshRenderer que solo este script usa y que no debe verse en el inspector
-    private MeshRenderer _mh;
+    private MeshRenderer _meshRenderer;
 
     //12° Un valor que controla el volumen del juego, editable desde el inspector y con un mensaje de ayuda
 
     [Tooltip("Ajuste de volumen")]
-    [SerializeField] internal int VolumeGame;
+    [SerializeField] internal float _volumeGame = 1f;
 
     //13° Un valor verdadero/falso que indica si el enemigo puede atacar. Solo este script lo usa
     private bool _canAttack;
 
     //14° La instancia global de GameManager a la que cualquier script puede acceder sin crearla
 
-    public static EjercicioDeVariables Instance { get; private set; }
-    //(Para aclarar, debería decir GameManager en vez de "EjercicioDeVariables", pero daba un errror así que así lo pondré para que no de el error
+    public static GameManager Instance;
 
     //15° Una lista para almacenar objetos del inventario. Solo este script la modifica
 
-    private List<string> _playerInventory = new List<string>();
+    private List<Item> _playerInventory = new List<Item>();
 
     //16°Un valor de velocidad que antes se llamaba "_speed", debe ser editable desde el editor.
 
-    [Obsolete] public float _speed;
+    [FormerlySerializedAs("_speed")]
+    [SerializeField] public float _renamedSpeed = 5f;
 
     //17° Una posicion del jugador usando Vector3, solo este script la usa.
 
@@ -77,15 +78,24 @@ public class EjercicioDeVariables : MonoBehaviour
 
     //18° La cantidad máxima de jugadores permitidos en la partida que un sistema deba consultar
 
-    internal int MaxPlayer;
+    public int MaxPlayer;
 
     //19° Una distancia de detección enemiga que las clases derivadas puedan reutilizar y que sea editable desde el inspector
 
-    [SerializeField] protected int EnemyDetection;
+    [SerializeField] protected float _enemyDetection = 15f; 
 
     //20° Una referencia a AudioSource que solo este script usa y que sea editable desde el inspector.
 
-    [SerializeField] private AudioSource _as;
+    [SerializeField] private AudioSource _audioSource;
 
+    public class GameManager : MonoBehaviour
+    {
 
+    }
+
+    [System.Serializable]
+    public class Item
+    {
+        public string Name;
+    }
 }
--- GameDesign/Assets/SergioMauricio/MetodosEjercicio.cs
+++ GameDesign/Assets/SergioMauricio/MetodosEjercicio.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using Unity.IO.LowLevel.Unsafe;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MetodosEjercicio : MonoBehaviour
@@ -30,10 +33,10 @@ public class MetodosEjercicio : MonoBehaviour
 
     //°4 Crea un método que devuelva la direccion normalizada desde un origen hasta su destino
 
-    private Vector3 NormalizeDirection (Vector3 _OriginPoint, Vector3 _destinyPoint)
+    private Vector3 NormalizeDirection (Vector3 _originPoint, Vector3 _destinyPoint)
     {
 
-        return Vector3.Normalize(_OriginPoint - _destinyPoint);
+        return Vector3.Normalize(_originPoint - _destinyPoint);
     }
     //5° Crea un método que devuelva el nombre actual del jugador
     /*
@@ -42,4 +45,152 @@ public class MetodosEjercicio : MonoBehaviour
         return _playerName.Name;
     }
     */
+
+    //6° Crea un metodo que cuente la cantidad de enemigos contenidos en una lista
+
+    private void EnemyList(List<int> _enemyList)
+    {
+        foreach (int enemy in _enemyList) 
+        {
+            int _enemyNumber =+ 1 ;
+        }
+    }
+
+    //7° Crea un metodo que encuentre al enemigo mas cercano al jugador dentro de una lista
+
+    private void FindEnemy(List<Vector3> _enemyList, Vector3 _playerPoint)
+    {
+        foreach (Vector3 _enemy in _enemyList) 
+        {
+            float _enemyDistance = Vector3.Distance(_enemy, _playerPoint);
+
+            float? _enemyNearby = null;
+            if ( _enemyDistance < _enemyNearby)
+            {
+                _enemyNearby = _enemyDistance;
+            }
+        }
+    }
+
+    //8° Crea un metodo que reciba velocidad y dirección y mueva al jugador
+
+    private void MovementPlayer(Vector3 _directionPlayer, float _velocityPlayer)
+    {
+        transform.position += _directionPlayer * _velocityPlayer * Time.deltaTime;
+    }
+
+    //9° Crea un metodo que convierta un angulo expresado en grados a radianes
+
+    private void GradesToRadian (double _angleInGrades)
+    {
+        double _angleInRadian = _angleInGrades * 3.1416 / 180;
+    }
+
+    //10°Crear un metodo que intente obtener al jugador mas cercano dentro de un rango dado, indicando si se encontró uno y devolviendo el jugador obtenido
+
+    private GameObject PlayerNearby(GameObject[] _playerList, float _inRange, out bool _playerFound)
+    {
+
+        GameObject _playerName = null;
+
+        foreach (GameObject _player in _playerList) 
+        {
+            float _playerDistance = Vector3.Distance(transform.position, _player.transform.position);
+
+            if (_playerDistance < _inRange)
+            {
+                _inRange = _playerDistance;
+                _playerName = _player;
+            }
+        }
+        _playerFound = _playerName != null;
+        return _playerName;
+    }
+
+    //11° Crea un metodo que intente convertir un texto a entero, indicando si la conversión fue exitosa y devolviendo el valor obtenido
+
+    private bool TextToInteger(string _originalText, out int _numberOut) 
+    {
+        return int.TryParse(_originalText, out _numberOut);
+    }
+
+    //12° Crea un metodo que reciba un ángulo en grados y devuelva la rotación correspondiente como cuaternion
+
+    private Quaternion RotationInQuaternion(float _gradesInAngle)
+    {
+        return Quaternion.Euler(0, _gradesInAngle, 0);
+    }
+
+    //13° Crea un metodo que llene una lista con todos los enemigos que se encuentren en un area dada
+    private void EnemyListInArea(GameObject[] _enemyList, int _inRange)
+    {
+        List<GameObject> _enemyInRange = new List<GameObject>();
+
+        foreach (GameObject _enemy in _enemyList) 
+        {
+            float _enemyDistance = Vector3.Distance(transform.position, _enemy.transform.position);
+            if (_enemyDistance < _inRange)
+            {
+                _enemyInRange.Add(_enemy);
+            }
+        }
+    }
+    //14° Crea un metodo que reinicie la posición del jugador a un punto de aparición.
+
+    private void CheckPoint (GameObject _playerPosition, GameObject _checkPointPosition)
+    {
+        _playerPosition.transform.position = _checkPointPosition.transform.position;
+    }
+    //15° En una clase base, crear un metodo diseñado para ser redefinido por las clases hijas
+    public class Character
+    {
+        public virtual void Attack()
+        {
+            int AttackValue = 10;
+        }
+    }
+    //16° En una clase hija, redefine el método del ejercicio anterior para proporcionar un comportamiento en específico.
+    public class Ninja : Character
+    {
+        public override void Attack()
+        {
+            int AttackValue = 5;
+        }
+
+        //17° En la clase hija llama al comportamiento original en el método definido en la clase base
+    }
+    public class Mage : Character
+    {
+        public override void Attack()
+        {
+            base.Attack();
+        }
+    }
+    //18° Crea un metodo que devuelva un porcentaje de  vida actual del
+    private int PlayerHealthPercentaje (int _actualHealth, int _maxHealth)
+    {
+        int _playerPercentaje;
+        return _playerPercentaje = _actualHealth / _maxHealth * 100;
+    }
+    //19° Crea un método que determine si un enemigo puede esquivar un ataque con una probabilidad dada
+    private void CanAvoid(int _enemyAvoid, int _hitPercentaje)
+    {
+        bool _canAvoid;
+
+        if (_enemyAvoid > _hitPercentaje)
+        {
+            _canAvoid = true;
+        }
+        else
+        {
+            _canAvoid = false;
+        }
+    }
+    //20° Crea un método que aplique una fuerza en una dirección a un RigidBody
+    private void ForceToDirection(Rigidbody _rigidBody, float _forceOfImpact, Vector3 _directionOfImpact)
+    {
+        _rigidBody.AddForce(_directionOfImpact * _forceOfImpact);
+    }
+
+
 }
