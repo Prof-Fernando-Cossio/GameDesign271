c42dd7367fab25a8d09883a58945d399cdb5616e
Otto <prograbugs@gmail.com>
2026-09-05 11:14:22 -0600

    Final Properties Practice

--- GameDesign/Assets/LuisOctavioRiveraCliment/Scripts/PropertiesPractice.cs
+++ GameDesign/Assets/LuisOctavioRiveraCliment/Scripts/PropertiesPractice.cs
@@ -1,5 +1,8 @@
 using UnityEngine;
 using System.Runtime.CompilerServices;
+using TMPro;
+using NUnit.Framework;
+using System.Collections.Generic;
 
 namespace System.Runtime.CompilerServices
 {
@@ -8,7 +11,17 @@ namespace System.Runtime.CompilerServices
 
 public class PropertiesPractice : MonoBehaviour
 {
+    private int _damage;
+    private float _attackRange;
+    private int _gameVolume;
+    private int _availableSpaces;
+    Player _player;
+    Rigidbody _rigidBody;
+    private List <string> _inventoryItems = new List<string>();
+    [SerializeField]private float _runningVelocity;
+
     private int _currentHealth, _maxHealth;
+
     //1
     public int Health { get; private set; }
 
@@ -27,4 +40,47 @@ public class PropertiesPractice : MonoBehaviour
     //6
     private float _movementSpeed;
     public float MovementSpeed => _movementSpeed;
+
+    //7
+    public int Damage { get { return _damage; } set { _damage = Mathf.Clamp(value, 0, 100); } }
+
+    //8
+    public static int PlayerCount {  get; private set; }
+
+    //9
+    public float Experience { get; private set; }
+
+    //10
+    [SerializeField] private int _stamina;
+    public int Stamina => _stamina;
+
+    //11
+    private bool _canAttack => Vector3.Distance(_player.transform.position, this.transform.position) <= _attackRange;
+
+    //12
+    public int GameVolume { get { return _gameVolume; } set { _gameVolume = Mathf.Clamp(value, 0, 100);} }
+
+    //13
+    public string CharacterCreationDate { get; }
+
+    //14
+    private bool _isInventoryFull => (int)_availableSpaces == 0;
+
+    //15
+    public int MaxLevel { get; init; }
+
+    //16
+    private float _actualVelocityX => _rigidBody.linearVelocity.x;
+
+    //17
+    public float Energy { get; private set; }
+
+    //18
+    public Vector3 ActualPosition { get { return transform.position; } }
+
+    //19
+    public List<string> InventoryItems { get { return _inventoryItems; }}
+
+    //20
+    private bool _isCharacterRunning => _rigidBody.linearVelocity.magnitude >= _runningVelocity;
 }
