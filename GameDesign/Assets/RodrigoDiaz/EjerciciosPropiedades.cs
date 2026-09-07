c1570e75d9519d52d23fede7adb23237ac6812c5
Roy <rodrigodj526@gmail.com>
2026-09-06 18:29:19 -0600

    Ejercicio Propiedades

---
+++ GameDesign/Assets/RodrigoDiaz/EjerciciosPropiedades.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using System;
+
+public class EjerciciosPropiedades : MonoBehaviour
+{
+    //1.
+    //public int Health {get; private set;}
+    //2.
+    //public bool isDead => Health <= 0;
+    //3.
+    //public string PlayerName {get; init;}
+    //4.
+    //public int Coins {get; set;}
+    //5.
+    //public float HealthPercentage => (float)_currentHealth / _maxHealth * 100f;
+    //6.
+    //private int _movementSpeed;
+    //public int movementSpeed => _movementSpeed;
+    //7.
+    //private int _damage;
+    //public int Damage {get {return _damage;} set {_damage = Mathf.Clamp(value, 0, 100);}}
+    //8.
+    //public static int ConnectedPlayers {get; set;}
+    //9.
+    //public int Experience {get; private set;}
+    //10.
+    //[SerializedField] private int _stamina;
+    //public int Stamina => _stamina;
+    //11.
+    //public bool canEnemyAttack => Vector3.Distance(this.Transform.position, Player.Transform.position) <= _attackRange;
+    //12.
+    //public int Volume {get; set {Volume = Mathg.Clamp(value, 0, 100);}}
+    //13.
+    //public DateTime CharacterCreationDate {get;} = DateTime.Now;
+    //14.
+    //public bool isInventoryFull => _inventoryUsage <= _inventoryCapacity;
+    //15.
+    //public int MaxLevel {get; init;}
+    //16.
+    //public float HorizontalSpeed => RigidBody.velocity.x;
+    //17.
+    //public int Energy {get; private set;}
+    //18.
+    //public Vector3 CurrentPosition {get {this.Transform.position;}}
+    //19.
+    //private List<Item> _inventoryItems = new List<Item>();
+    //public List<Item> InventoryItems {get;} = _inventoryItems;
+    //20.
+    //public bool isPlayerRunning => _movementSpeed <= 20;
+}
